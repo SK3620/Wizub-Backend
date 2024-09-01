d25f07219b3a8938d8f4f752c7374ebfc075a182
SK3620 <k.n.t11193620@gmail.com>
2024-09-01 20:44:29 +0900

    Add: 動画取得リクエストを行うAPIのURLを定義

--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,5 @@ Route::middleware('api')->post('/check_email', 'App\Http\Controllers\AuthControl
 Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('api')->get('/get_transcript', 'App\Http\Controllers\TranscriptController@getTranscript');
     Route::middleware('api')->get('/get_open_ai_answer', 'App\Http\Controllers\OpenAIController@getOpenAIResponse');
+    Route::middleware('api')->get('/search', 'App\Http\Controllers\YouTubeController@search');
 });
