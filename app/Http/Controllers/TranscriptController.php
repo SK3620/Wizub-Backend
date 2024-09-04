64245bc6bc8b2e772e094e688fe5f8a1e2a90ea0
SK3620 <k.n.t11193620@gmail.com>
2024-09-04 23:09:17 +0900

    Add: すでに保存済みのトランスクリプトを取得する処理を定義

--- app/Http/Controllers/TranscriptController.php
+++ app/Http/Controllers/TranscriptController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class TranscriptController extends Controller
 {
@@ -25,5 +27,38 @@ class TranscriptController extends Controller
 
         $transcript = $process->getOutput();
         return response()->json(json_decode($transcript, true));
+    }
+
+    // すでに保存済みのトランスクリプトを取得
+    public function getSavedTranscript(Request $request)
+    {
+        // Userを取得
+        $user = Auth::user();
+
+        // リクエストからvideo_idを取得
+        $video_id = $request->query('video_id');
+
+        //video_idでVideoモデルを検索し、そのトランスクリプトを取得
+        $video = $user->videos()->where('video_id', $video_id)->with('transcripts')->first();
+
+        // videoが存在しない場合、エラーレスポンスを返す
+        if (!$video) {
+            return response()->json(['error' => 'Video not found or not owned by the user'], 404);
+        }
+
+        // トランスクリプトのフォーマット調整
+        $transcripts = $video->transcripts->map(function ($transcript) {
+            return [
+                'id' => $transcript->id, // モデルのプライマリーキー
+                'transcript_id' => $transcript->transcript_id, // それぞれの動画のトランスクリプトのID
+                'en_subtitle' => $transcript->en_subtitle ?? '', // 英語字幕
+                'ja_subtitle' => $transcript->ja_subtitle ?? '', // 日本語字幕
+                'start' => $transcript->start, // 字幕表示開始時間
+                'duration' => $transcript->duration, // 字幕表示時間
+            ];
+        });
+
+        // レスポンスを返す
+        return response()->json(['transcripts' => $transcripts]);
     }
 }
