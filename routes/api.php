50ea86c62cd1b16d7800c8dd9001f30d8fe4c662
SK3620 <k.n.t11193620@gmail.com>
2024-09-05 23:02:34 +0900

    Add: 保存した動画の字幕情報を更新するAPIのルートを定義

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::middleware('api')->post('/check_email', 'App\Http\Controllers\AuthControl
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('api')->get('/get_transcript', 'App\Http\Controllers\TranscriptController@getTranscript');
+    Route::middleware('api')->put('/update_transcripts', 'App\Http\Controllers\TranscriptController@update');
     Route::middleware('api')->get('/get_open_ai_answer', 'App\Http\Controllers\OpenAIController@getOpenAIResponse');
     Route::middleware('api')->get('/search', 'App\Http\Controllers\YouTubeController@search');
     Route::middleware('api')->post('/store_transcripts', 'App\Http\Controllers\VideoController@store');
