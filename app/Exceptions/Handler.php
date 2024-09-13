86b57034adf03085c6230acba31fd045f0869d43
SK3620 <k.n.t11193620@gmail.com>
2024-09-13 14:26:25 +0900

    HTTPエラーに対する詳細なエラーレスポンスを実装

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -29,5 +29,37 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    // HTTPエラー
+    private function apiErrorResponse($request, $exception)
+    {
+        // HttpExceptionが発生しているかどうかを確認
+        if ($this->isHttpException($exception)) {
+            // 例外からステータスコード取得
+            $statusCode = $exception->getStatusCode();
+
+            // ApiServiceProviderで作成したレスポンスマクロ'error'を呼ぶ
+            switch ($statusCode) {
+                    // ステータスコードを表す定数名"$status"とエラーメッセージ"$message"を指定
+                case 400:
+                    return response()->error(Response::HTTP_BAD_REQUEST, '不正なリクエストです。');
+                case 401:
+                    return response()->error(Response::HTTP_UNAUTHORIZED, '認証エラーが発生しました。');
+                case 403:
+                    return response()->error(Response::HTTP_FORBIDDEN, 'アクセスエラーが発生しました。');
+                case 404:
+                    return response()->error(Response::HTTP_NOT_FOUND, '存在しないURLです。');
+                case 405:
+                    return response()->error(Response::HTTP_METHOD_NOT_ALLOWED, '無効なリクエストです。');
+                case 406:
+                    return response()->error(Response::HTTP_NOT_ACCEPTABLE, '受付不可能なリクエスト値です。');
+                case 408:
+                    return response()->error(Response::HTTP_REQUEST_TIMEOUT, 'リクエストがタイムアウトしました。');
+                    // 500系エラー（500〜599をまとめて処理）
+                case ($statusCode >= 500 && $statusCode <= 599):
+                    return response()->error($statusCode, 'サーバーでエラーが発生しました。');
+                default:
+                    return response()->error(self::UNKNOWN, 'サーバーで不明なエラーが発生しました。');
+            }
+        }
     }
 }
