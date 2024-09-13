4549d0913a8b04ddaf50c61e294afe84eb0abcb8
SK3620 <k.n.t11193620@gmail.com>
2024-09-13 09:40:48 +0900

    Add: レスポンスマクロを用いて、Responseに'error'というカスタムメソッドを追加し、一貫したエラーレスポンスを返す処理を追加

---
+++ app/Providers/ApiResponseServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
+
+class ApiResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // レスポンスマクロを用いて、Responseに'error'というカスタムメソッドを追加し、一貫したエラーレスポンスを返す
+        Response::macro('error', function ($status, $message) {
+            return response()->json([
+                'code' => $status,
+                'message' => $message,
+            ], $status);
+        });
+    }
+}
--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ApiResponseServiceProvider::class, // サービスプロバイダを登録し、laravelアプリケーション内で使用可能にする
     ])->toArray(),
 
     /*
