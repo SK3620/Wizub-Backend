62c8c85304bf1cab3e09757d6bd9bf005d8c8c84
SK3620 <k.n.t11193620@gmail.com>
2024-09-13 16:14:57 +0900

    Clean: リファクタリング

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,37 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     public function signUp(Request $request)
     {
-        // email重複以外のバリデーションはアプリ側で細かく実装しているため、laravel側では実際は不要
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-        ]);
+        // アプリ側でバリデーションを実装しているためlaravel側でのバリデーションは不要
 
         $user = User::create(['name' => $request->name, 'email' => $request->email, 'password' => Hash::make($request->password)]);
 
         $token = $user->createToken('api_token')->plainTextToken;
 
-        return [
+        return response()->json([
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password,
             'is_duplicated_email' => null,
             'api_token' => $token
-        ];
+        ]);
     }
 
     public function signIn(Request $request)
     {
-        $request->validate([
-            'email' => 'required',
-            'password' => 'required',
-        ]);
+        // アプリ側でバリデーションを実装しているためlaravel側でのバリデーションは不要
 
         // usersテーブルから指定されたemailカラムの値に一致する最初のレコードを取得
         $user = User::where('email', $request->email)->first();
@@ -47,18 +40,20 @@ class AuthController extends Controller
 
         $token = $user->createToken('api_token')->plainTextToken;
 
-        return [
+        return response()->json([
             'name' => '',
             'email' => $request->email,
             'password' => $request->password,
             'is_duplicated_email' => null,
             'api_token' => $token
-        ];
+        ]);
     }
 
     // Emailの重複チェック
     public function checkEmail(Request $request)
     {
+        // アプリ側でバリデーションを実装しているためlaravel側でのバリデーションは不要
+
         // メールアドレスが重複しているか確認
         $emailExists = User::where('email', $request->email)->exists();
 
