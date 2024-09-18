07e4a3d1b3e3882043a78b52efa641bc9e4fa8ec
SK3620 <k.n.t11193620@gmail.com>
2024-09-18 19:40:24 +0900

    Update: 翻訳する字幕数の制限エラーとタイムアウトエラーのハンドリング処理を追加

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -2,25 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Exceptions\OpenAIException;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Illuminate\Support\Facades\Log;
+use App\Exceptions\VideoSubtitleException;
+use Illuminate\Http\Request;
+use Exception;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 class OpenAIController extends Controller
 {
-    public function getOpenAIResponse(Request $request)
+    public function translateSubtitles(Request $request)
     {
-        // 質問内容 取得
+        // 翻訳する英語字幕を格納する配列
         $content = $request->query('content');
+        Log::debug('翻訳する英文↓');
+        Log::debug($content);
+        // 配列の要素数
+        $arrayCount = $request->query('array_count');
+
         // OpenAI APIキー取得
         $apiKey = config('services.open_ai.api_key');
 
         // Pythonスクリプトのパス
         $scriptPath = base_path('myenv/scripts/get_open_ai_answer.py');
 
-        // Pythonスクリプトを実行 
-        $process = new Process(['python3', $scriptPath, $apiKey, $content]);
-        $process->run();
+        try {
+            // 配列の要素数を用いて、翻訳量の制限を判定
+            if ($arrayCount > 100) {
+                throw new OpenAIException(message: '翻訳する字幕数が多すぎます。\n翻訳したい字幕を最大100個まで選択して翻訳を行なってください。', detail: 'Too Many Subtitles');
+            }
+
+            // Pythonスクリプトを実行 
+            $process = new Process(['python3', $scriptPath, $apiKey, $content]);
+            // タイムアウトはデフォルト値30秒→60秒へ変更
+            $process->setTimeout(60);
+            $process->run();
+
+            // プロセスの成功を確認
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process); // プロセス失敗時に例外をスロー
+            }
+
+            // レスポンスを取得し、"answer"キーでラップして返す
+            $response = $process->getOutput();
+            Log::debug('ChatGPT翻訳結果↓');
+            Log::debug($response);
+            return response()->json(['answer' => json_decode($response, true)]);
+        } catch (OpenAIException $e) {
+            Log::error('Too Many Subtitles: ' . $e->getMessage());
+            throw $e;
+        } catch (ProcessTimedOutException $e) {
+            Log::error('Process Timed Out Exception: ' . $e->getMessage());
+            throw new OpenAIException(message: '翻訳処理がタイムアウトしました。\n再度お試し頂くか、翻訳したい字幕を最大100個まで選択して翻訳を行なってください。', detail: $e->getMessage());
+        } catch (ProcessFailedException $e) {
+            // 失敗の例外処理
+            Log::error('Process Failed Exception: ' . $e->getMessage());
+            throw new OpenAIException(message: '字幕の翻訳に失敗しました。\n再度、お試しください。', detail: $e->getMessage());
+        } catch (Exception $e) {
+            // その他のエラー
+            throw $e;
+        }
+    }
+
     /* // （並行処理による字幕翻訳処理時に使用）※翻訳精度の極度な不安定性により使用中断中
     public function translateSubtitles(TranslateSubtitlesRequest $request)
     {
