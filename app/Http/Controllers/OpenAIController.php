68aa3a200c7066955722ebdeb81914aa54a10a08
SK3620 <k.n.t11193620@gmail.com>
2024-09-24 15:24:49 +0900

    Clean: 不要なコードを削除/エラーメッセージの変更

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -29,15 +29,8 @@ class OpenAIController extends Controller
         $scriptPath = base_path('myenv/scripts/get_open_ai_answer.py');
 
         try {
-            // 配列の要素数を用いて、翻訳量の制限を判定
-            // if ($totalSubtitlesCount > 100) {
-            //     throw new OpenAIException(message: '翻訳する字幕数が多すぎます。\n翻訳したい字幕を最大100個まで選択して翻訳を行なってください。', detail: 'Too Many Subtitles');
-            // }
-
             // Pythonスクリプトを実行 
             $process = new Process(['python3', $scriptPath, $apiKey, $content]);
-            // タイムアウトはデフォルト値30秒→60秒へ変更
-            $process->setTimeout(60);
             $process->run();
 
             // プロセスの成功を確認
@@ -50,16 +43,13 @@ class OpenAIController extends Controller
             Log::debug('ChatGPT翻訳結果↓');
             Log::debug($response);
             return response()->json(['answer' => json_decode($response, true)]);
-        } catch (OpenAIException $e) {
-            Log::error('Too Many Subtitles: ' . $e->getMessage());
-            throw $e;
         } catch (ProcessTimedOutException $e) {
             Log::error('Process Timed Out Exception: ' . $e->getMessage());
-            throw new OpenAIException(message: '翻訳処理がタイムアウトしました。\n再度お試し頂くか、翻訳したい字幕を最大100個まで選択して翻訳を行なってください。', detail: $e->getMessage());
+            throw new OpenAIException(message: '翻訳処理がタイムアウトしました。\n再度お試しください。', detail: $e->getMessage());
         } catch (ProcessFailedException $e) {
             // 失敗の例外処理
             Log::error('Process Failed Exception: ' . $e->getMessage());
-            throw new OpenAIException(message: '字幕の翻訳に失敗しました。\n再度、お試しください。', detail: $e->getMessage());
+            throw new OpenAIException(message: '字幕の翻訳に失敗しました。\n再度お試しください。', detail: $e->getMessage());
         } catch (Exception $e) {
             // その他のエラー
             throw $e;
